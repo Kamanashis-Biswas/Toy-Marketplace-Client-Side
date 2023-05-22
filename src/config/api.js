ab6572248cfec955825fcb20942f8f098a38d3b9
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-05-23 01:06:04 +0600

    Final Submission

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api/",
-    //baseURL: "https://assignment-11-server-side-kamanashiscse.vercel.app/api/",
+    //baseURL: "http://localhost:5000/api/",
+    baseURL: "https://assignment-11-server-side-kamanashiscse.vercel.app/api/",
     withCredentials: true,
     headers: {
             Authorization : `Bearer ${localStorage.getItem("access_token")}`
