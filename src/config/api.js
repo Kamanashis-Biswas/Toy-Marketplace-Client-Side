2f60f9899f1940df6177a07bc9e961ea4fdd44b4
Kamanashis Biswas <59314880+Kamanashis-Biswas@users.noreply.github.com>
2023-08-05 02:30:11 +0600

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     //baseURL: "http://localhost:5000/api/",
-    baseURL: "https://assignment-11-server-side-kamanashiscse.vercel.app/api/",
+    baseURL: "https://toy-marketplace-server-side-3.vercel.app/api",
     withCredentials: true,
     headers: {
             Authorization : `Bearer ${localStorage.getItem("access_token")}`
